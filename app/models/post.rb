a08d1e3e8f21039281d1f8cba6f65881cf1f8d08
Mauricio Mendoza <rbreva@gmail.com>
2023-01-20 20:04:56 -0500

    models added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  
-  belongs_to :user
+
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_posts_counter
+
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
