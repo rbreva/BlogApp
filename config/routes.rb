7faf9c0d298914bac0e9a150cc9249d96693c588
Mauricio Mendoza <rbreva@gmail.com>
2023-01-24 15:07:06 -0500

    Blog app - controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 
+  root 'users#index'
+  #get "/users/:id", to: "users#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
