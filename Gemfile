3ef698e0ed5b121a49fd9bf9713c5d6130c258d0
Mauricio Mendoza <rbreva@gmail.com>
2023-01-25 16:17:57 -0500

    changes required

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails', '~> 6.0', '>= 6.0.1'
 end
 
 group :development do
@@ -76,5 +77,3 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
-
-gem 'rspec-rails', '~> 6.0', '>= 6.0.1'
--- spec/controllers/post_spec.rb
+++ spec/controllers/post_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-  it 'Expect a response code 200 and render correctly from the view index' do
+  it 'Expect render index' do
     get '/users/1/posts'
     expect(response).to render_template(:index)
     expect(response).to have_http_status(200)
     expect(response.body).to include('User Number One')
   end
 
-  it 'Expect a response code 200 and render correctly from the view show' do
+  it 'Expect render from Show' do
     get '/users/1/posts/1'
     expect(response).to render_template(:show)
     expect(response).to have_http_status(200)
